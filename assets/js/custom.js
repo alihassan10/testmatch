f9db71286f907e67aff239241c05e1529c8bc264
alihassan10 <neymarneymar1100@gmail.com>
2021-01-28 00:36:18 +0500

    jan281

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,12 +17,31 @@ $('#main-carousel').owlCarousel({
     }
 })
 
-$('#main-testnomial').owlCarousel({
+$('#main-testimonial').owlCarousel({
     loop:true,
     nav:false,
     dots:false,
     autoplay:true,
-    autoplayTimeout:155000,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#main-client').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2500,
     responsive:{
         0:{
             items:1
